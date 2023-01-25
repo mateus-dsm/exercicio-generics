b32d9604b3ff8138942c6070810c13390efadeb4
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2023-01-25 17:38:58 -0300

    Add files via upload

---
+++ Parte1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public static bool CompareObject(string string1, string string2)
+    {
+        if(string1.Equals(string2)) return true;
+        return false;
+    }
+
+    public static bool CompareObject(int int1, int int2)
+    {
+        if(int1 == int2) return true;
+        return false;
+    }
+
+    public static bool CompareObject(double double1, double double2)
+    {
+        if(double1 == double2) return true;
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CompareObject("Mateus", "Mateus"));
+        Console.WriteLine(CompareObject("Mateus", "Matheus")); //Meu nome NAO TEM H!!!!!
+        Console.WriteLine(CompareObject(1, 1));
+        Console.WriteLine(CompareObject(1, 0));
+        Console.WriteLine(CompareObject(3.14, 3.14));
+        Console.WriteLine(CompareObject(3.14, 3.15));
+    }
+}
